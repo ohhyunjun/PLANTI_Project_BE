ee0056720a9dd05e23338a5a9c5a9087315c3e6f
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-18 16:24:37 +0900

    chore: First init

---
+++ src/main/java/com/metaverse/planti_be/PlantiBeApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.planti_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metaverse/planti_be/PlantiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.planti_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
