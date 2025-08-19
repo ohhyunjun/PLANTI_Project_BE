3cfe8a16b7f793821fe75e4b8cfdc2153d1c441c
JDoyoung <airwalk6346@gmail.com>
2025-08-19 12:01:22 +0900

    feat: PostController CRUD 기초 구현

--- src/main/java/com/metaverse/planti_be/post/controller/PostController.java
+++ src/main/java/com/metaverse/planti_be/post/controller/PostController.java
@@ -2,13 +2,14 @@ package com.metaverse.planti_be.post.controller;
 
 import com.metaverse.planti_be.post.domain.Post;
 import com.metaverse.planti_be.post.dto.PostRequestDto;
+import com.metaverse.planti_be.post.dto.PostResponseDto;
 import com.metaverse.planti_be.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +21,29 @@ public class PostController {
     // 게시글 작성 API
     public ResponseEntity<Post> createPost(@RequestBody PostRequestDto postRequestDto){
         Post post = postService.save(postRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new PostResponseDto(post));
+    }
+
+    // 게시글 목록 조회 API
+    @GetMapping("/posts")
+    public ResponseEntity<List<PostResponseDto>> getAllPosts(){
+        List<PostListResponseDto> postListDto = postService.findAll()
+                .stream()
+                .map(PostListResponseDto::new)
+                .toList();
+        return ResponseEntity.ok().body(postListDto);
+    }
+
+    // 게시글 삭제 API
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<Void> deletePostById(@PathVariable Long id){
+        postService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    // 게시글 수정 API
+    public ResponseEntity<PostResponseDto> updatePostById(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto){
+        Post post = postService.update(id, postRequestDto);
+        return ResponseEntity.ok().body(new PostResponseDto(post));
     }
 }
