a97923ace0b3dc1f1384e7545bbc5cb57cbfe838
JDoyoung <airwalk6346@gmail.com>
2025-08-19 11:30:32 +0900

    feat: PostService 기초 구현

---
+++ src/main/java/com/metaverse/planti_be/post/service/PostService.java
@@ -0,0 +1,18 @@
+package com.metaverse.planti_be.post.service;
+
+import com.metaverse.planti_be.post.domain.Post;
+import com.metaverse.planti_be.post.dto.PostRequestDto;
+import com.metaverse.planti_be.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public Post save(PostRequestDto dto){
+        return postRepository.save(dto.toEntity());
+    }
+}
