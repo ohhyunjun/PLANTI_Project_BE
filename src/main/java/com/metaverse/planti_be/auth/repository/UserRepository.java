5d1ad900133caa137f4c0419cb43eb8b61509808
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-19 16:58:24 +0900

    feat: 유저레포지토리 작성

---
+++ src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.metaverse.planti_be.auth.repository;
+
+import com.metaverse.planti_be.auth.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
