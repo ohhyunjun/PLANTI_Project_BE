b798ebd69fc13bec775519262a5dece293ce1952
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-19 17:27:55 +0900

    feat: UserRepository에 existsByName 메소드 추가

--- src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
+++ src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.metaverse.planti_be.auth.repository;
 import com.metaverse.planti_be.auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    Boolean existsByName(String name);
 }
