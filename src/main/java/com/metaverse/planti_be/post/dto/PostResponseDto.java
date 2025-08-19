27343ff1086aa8a38d4580ef90595dea2ae27cd9
JDoyoung <airwalk6346@gmail.com>
2025-08-19 10:30:22 +0900

    feat: PostResponseDto Post단계에서만 구현 (cause, 아직 Comment와 User 미구현)

---
+++ src/main/java/com/metaverse/planti_be/post/dto/PostResponseDto.java
@@ -0,0 +1,30 @@
+package com.metaverse.planti_be.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.metaverse.planti_be.post.domain.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.createdAt = post.getCreatedAt();
+        this.updatedAt = post.getUpdateAt();
+    }
+
+}
