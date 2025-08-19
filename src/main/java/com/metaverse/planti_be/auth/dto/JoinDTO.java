c63f09a3c9994e5fa22d29801a76d553945f0d6b
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-19 17:27:33 +0900

    feat: 회원가입 요청을 위한 JoinDTO 추가

---
+++ src/main/java/com/metaverse/planti_be/auth/dto/JoinDTO.java
@@ -0,0 +1,12 @@
+package com.metaverse.planti_be.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class JoinDTO {
+
+    private String name;
+    private String password;
+}
