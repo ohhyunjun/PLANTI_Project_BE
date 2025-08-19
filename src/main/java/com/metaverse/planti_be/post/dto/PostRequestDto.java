ce27174481390a6839b9620c0f9878ae773aa4f3
JDoyoung <airwalk6346@gmail.com>
2025-08-19 10:20:43 +0900

    feat: PostRequestDto 구현

---
+++ src/main/java/com/metaverse/planti_be/post/dto/PostRequestDto.java
@@ -0,0 +1,10 @@
+package com.metaverse.planti_be.post.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostRequestDto {
+    private Long id;
+    private String title;
+    private String content;
+}
