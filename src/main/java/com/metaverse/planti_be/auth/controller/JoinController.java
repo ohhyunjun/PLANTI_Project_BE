c40c7e1382a5b9825973d30d45054fb522055777
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-19 17:27:46 +0900

    feat: 회원가입 API 엔드포인트 JoinController 추가

---
+++ src/main/java/com/metaverse/planti_be/auth/controller/JoinController.java
@@ -0,0 +1,27 @@
+package com.metaverse.planti_be.auth.controller;
+
+import com.metaverse.planti_be.auth.dto.JoinDTO;
+import com.metaverse.planti_be.auth.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        System.out.println(joinDTO.getName()); // ✅ getUsername() -> getName() 으로 변경
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
