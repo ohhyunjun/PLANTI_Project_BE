08b409e11434b2a0b2692dfd5c9e39055e71214a
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-20 11:18:00 +0900

    refactor: UserDetailsService 구현 방식 개선

--- src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
+++ src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Boolean existsByName(String name);
+
+    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    UserEntity findByName(String name);
 }
---
+++ src/main/java/com/metaverse/planti_be/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.metaverse.planti_be.auth.service;
+
+import com.metaverse.planti_be.auth.entity.UserEntity;
+import com.metaverse.planti_be.auth.repository.UserRepository;
+import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        UserEntity userData = userRepository.findByName(username);
+
+        if (userData != null) {
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
