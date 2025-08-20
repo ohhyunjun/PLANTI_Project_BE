96d82ca336f51c0e725b4b1a19cfe6286cf0f99e
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-20 20:48:20 +0900

    feat: 이메일 존재 중복 검증 추가

--- src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
+++ src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
@@ -2,11 +2,19 @@ package com.metaverse.planti_be.auth.repository;
 
 import com.metaverse.planti_be.auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    // 사용자 이름(name)으로 User 객체를 조회하는 메서드
+    // Spring Security의 UserDetailsService에서 사용
+    Optional<UserEntity> findByUsername(String name);
 
+    // 사용자 이름(username)이 존재하는지 확인하는 메서드 (회원가입 시 중복 체크 등)
     Boolean existsByName(String name);
 
-    //username을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
-    UserEntity findByName(String name);
+    // 이메일(email)이 존재하는지 확인하는 메서드 (회원가입 시 중복 체크 등)
+    boolean existsByEmail(String email);
 }
