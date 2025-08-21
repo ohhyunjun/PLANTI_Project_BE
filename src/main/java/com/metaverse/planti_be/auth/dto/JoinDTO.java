f68517846d2c4fa8be3a3b76f2a1e82fa1045305
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-21 12:18:54 +0900

    feat: 회원가입과 로그인의 요청 과 응답(json 방식)을 위한 DTO 구현

--- src/main/java/com/metaverse/planti_be/auth/dto/JoinDTO.java
+++ src/main/java/com/metaverse/planti_be/auth/dto/JoinDTO.java
@@ -1,5 +1,8 @@
 package com.metaverse.planti_be.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +10,15 @@ import lombok.Setter;
 @Getter
 public class JoinDTO {
 
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @Size(min = 4, max = 20, message = "아이디는 4자 이상 20자 이하로 입력해주세요.")
     private String name;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
     private String password;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
     private String email;
 }
