e8ca7a1254ae7e20bc410065e447682b8c6fc7ea
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-21 11:42:37 +0900

    fix: 오타 수정

--- src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
+++ src/main/java/com/metaverse/planti_be/auth/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     // 사용자 이름(name)으로 User 객체를 조회하는 메서드
     // Spring Security의 UserDetailsService에서 사용
-    Optional<UserEntity> findByUsername(String name);
+    Optional<UserEntity> findByUsername(String username);
 
     // 사용자 이름(username)이 존재하는지 확인하는 메서드 (회원가입 시 중복 체크 등)
     Boolean existsByName(String name);
