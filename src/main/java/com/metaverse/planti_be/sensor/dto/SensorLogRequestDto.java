f89679ec169ef0887e593852a24a84b38b45b3e2
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-24 23:50:39 +0900

    feat: JSON으로 받을 데이터에 대해 DTO 구현

---
+++ src/main/java/com/metaverse/planti_be/sensor/dto/SensorLogRequestDto.java
@@ -0,0 +1,14 @@
+package com.metaverse.planti_be.sensor.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SensorLogRequestDto {
+    private String sensor_type;
+    private String value;
+    private String serial_number;
+}
