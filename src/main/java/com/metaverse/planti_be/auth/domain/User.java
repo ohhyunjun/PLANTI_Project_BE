b67b83b80c602b648ebe71d75d393fd0393ec47e
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-24 23:48:43 +0900

    refactor: name을 모두 username으로 통일시킴

--- src/main/java/com/metaverse/planti_be/auth/domain/User.java
+++ src/main/java/com/metaverse/planti_be/auth/domain/User.java
@@ -5,13 +5,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 
