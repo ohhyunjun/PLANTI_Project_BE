52e8b5349887d06539b27badde16c2a5887987b6
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-27 23:07:43 +0900

    feat: 디바이스에 관한 엔터티 구체화 및 유저와 조인형성

--- src/main/java/com/metaverse/planti_be/device/domain/Device.java
+++ src/main/java/com/metaverse/planti_be/device/domain/Device.java
@@ -1,19 +1,31 @@
 package com.metaverse.planti_be.device.domain;
 
+import com.metaverse.planti_be.auth.domain.User;
+import com.metaverse.planti_be.common.TimeStamped;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.*;
 
+@Entity
+@Table(name = "device")
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "Device")
-@Entity
-public class Device {
-
+public class Device extends TimeStamped {
     @Id
-    @Column(length = 100)
-    private String serial_number;
+    @Column(name = "serial_number")
+    private String id;
+
+    @Column(name="device_nickname")
+    private String deviceNickname;
+
+    @Column(name="status")
+    private Boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY) // N:1 관계 명시, 성능을 위해 LAZY 로딩을 권장
+    @JoinColumn(name = "user_id")      // DB에 생성될 외래 키 컬럼 이름을 'user_id'로 지정
+    private User user;
+
 
 }
