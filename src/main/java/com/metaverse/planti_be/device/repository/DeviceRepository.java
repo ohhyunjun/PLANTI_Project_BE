a149cf87dddfc9c0f28775ee5df93a865e8fec37
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-27 23:08:28 +0900

    feat: 디바이스 레포지토리 구현

--- src/main/java/com/metaverse/planti_be/device/repository/DeviceRepository.java
+++ src/main/java/com/metaverse/planti_be/device/repository/DeviceRepository.java
@@ -2,6 +2,8 @@ package com.metaverse.planti_be.device.repository;
 
 import com.metaverse.planti_be.device.domain.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeviceRepository extends JpaRepository<Device, String> {
 }
