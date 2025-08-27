e213a7b7576f096ae206160d6a68e90f63066aa0
Hynjun_Oh <dhguswns75@gmail.com>
2025-08-28 00:22:55 +0900

    feat: Device 엔티티에 Led와의 1:1 관계를 추가

--- src/main/java/com/metaverse/planti_be/device/domain/Device.java
+++ src/main/java/com/metaverse/planti_be/device/domain/Device.java
@@ -2,6 +2,7 @@ package com.metaverse.planti_be.device.domain;
 
 import com.metaverse.planti_be.auth.domain.User;
 import com.metaverse.planti_be.common.TimeStamped;
+import com.metaverse.planti_be.led.domain.Led;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,5 +28,20 @@ public class Device extends TimeStamped {
     @JoinColumn(name = "user_id")      // DB에 생성될 외래 키 컬럼 이름을 'user_id'로 지정
     private User user;
 
+    @OneToOne(mappedBy = "device", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Led led;
+
+    // Led를 함께 생성하기 위한 편의 메서드
+    public void createLed() {
+        this.led = new Led(this);
+    }
+
+    // 관리자가 기기를 생성할 때 사용할 생성자
+    public Device(String id, String deviceNickname) {
+        this.id = id;
+        this.deviceNickname = deviceNickname;
+        this.status = false;
+        createLed(); // 생성 시점에 Led도 함께 생성
+    }
 
 }
